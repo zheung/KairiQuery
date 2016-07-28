f4a7756fb35e2eda00cf5f418a0d3eb4b69db1fc
DanoR <zheung@vip.qq.com>
2016-07-28 12:35:20 +0800

    添加解析公共辞典(WIP); 添加skill文件词典

---
+++ raw1/_dicter.js
@@ -0,0 +1,44 @@
+module.exports = {
+	rare: {// 稀有度
+		'NORMAL': 1,
+		'HIGHNORMAL': 2,
+		'RARE': 3,
+		'SUPERRARE': 4,
+		'ULTRARARE': 5,
+		'MILLIONRARE': 6
+	},
+	size: {// 图片大小
+		'S': 1,
+		'M': 2,
+		'L': 3
+	},
+	sex: {// 性别
+		'MALE': 1,
+		'FEMALE': 2,
+		'INTERSEX': 3,
+		'UNKNOWN': 4
+	},
+	hand: {// 惯用手
+		'RIGHT': 1,
+		'LEFT': 2,
+		'AMBIDEXTER': 3,
+		'UNKOWN': 4
+	},
+	uses: {// 用途
+		'CORE': 1,
+		'EXP': 2,
+		'EVO': 3,
+		'GOLD': 4,
+		'FAME': 5,
+		'SELL': 6
+	},
+	skillKind: {
+		'ATTACK': 1,
+		'SORCERY': 2,
+		'RECOVERY': 3,
+		'SUPPORT': 4,
+		'DEFENSE': 5,
+		'JAMMING': 6,
+		'SPECIAL': 7
+	}
+};
