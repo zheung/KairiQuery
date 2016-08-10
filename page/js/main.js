93e2a3de0615afab175da4010c03332c50a5dd1b
DanoR <zheung@vip.qq.com>
2016-08-10 11:52:08 +0800

    webpage making

--- page/js/main.js
+++ page/js/main.js
@@ -1,10 +1,33 @@
-var btn = $('.Button.On, .Button.Off');
-
+var btn = $('.Button.on, .Button.off');
 btn.mouseenter(function(e) {
 	if(e.altKey)
-		$(this).toggleClass('On').toggleClass('Off');
+		$(this).toggleClass('on').toggleClass('off');
 });
 
 btn.click(function() {
-	$(this).toggleClass('On').toggleClass('Off');
+	$(this).toggleClass('on').toggleClass('off');
+});
+
+var tabItems = $('.TabItem'), tabHeads = $('.TabHead');
+tabHeads.click(function() {
+	var id = this.dataset.id;
+	if(id) {
+		tabHeads.removeClass('active');
+		$(this).addClass('active');
+
+		tabItems.addClass('hide');
+		tabItems.filter('#Navi'+id).removeClass('hide');
+	}
+});
+
+$(document).keydown(function(e) {
+	if(e.keyCode == 9) {
+		var next = tabHeads.filter('.active')[e.shiftKey?'prev':'next']();
+
+		if(!next.length) next = tabHeads[e.shiftKey?'last':'first']();
+
+		next.click();
+
+		return false;
+	}
 });
