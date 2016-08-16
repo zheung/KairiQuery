8c689b9dbab9be4fdc5a4d2b448628ef9f4b90bf
DanoR <zheung@vip.qq.com>
2016-08-16 14:46:41 +0800

    upload tweak

--- upload.js
+++ upload.js
@@ -1,15 +1,15 @@
 var http = require('http'), fs = require('fs');
 
 var conf = {
-	file: './test.jpg'
+	file: './test.jpg',
 	cookie: 'SUB=_2A256tuCKDeTxGedI7lAT8CbPyDuIHXVZwlVCrDV8PUNbmtBeLRbtkW997byBiVvLOWKxqzGSrT0KJ_9wOw..;'
-}
+};
 
 var req = http.request({
 	method: 'POST',
 	host: 'picupload.service.weibo.com',
 	path: '/interface/pic_upload.php?app=miniblog&data=1&mime=image/jpeg',
-	headers: { "Cookie": conf.cookie }
+	headers: { 'Cookie': conf.cookie }
 }, function (res) {
 	res.setEncoding('utf8');
 
