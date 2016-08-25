d9a7d517e66105ea1262a88fbf71d96ccf27129a
DanoR <zheung@vip.qq.com>
2016-08-25 16:02:09 +0800

    filter improve!

--- dict/pict.js
+++ dict/pict.js
@@ -1,3 +1,5 @@
 module.exports = {
-	'10000011': ['627ab2d1gw1f6vn5r5abyj2074074mxx']
+	'10000011': ['627ab2d1gw1f6vn5r5abyj2074074mxx'],
+	'10114013': ['627ab2d1gw1f761tioj7bj20460460sy'],
+	'10114014': ['627ab2d1gw1f761m0371gj2046046dg1']
 };
