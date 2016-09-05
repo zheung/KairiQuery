c760d07e8d4bf3a1268735d24308cd81a68c4a5e
DanoR <zheung@vip.qq.com>
2016-09-06 00:34:04 +0800

    render/page improve

--- render/skill.js
+++ render/skill.js
@@ -8,7 +8,12 @@ module.exports = (card) => {
 	for(let st of skillTypes) {
 		for(let skill of skills[st]) {
 			if(skill.cond.type) {
-				true;
+				if(skill.cond.type == 1) {
+					if(~~skill.cond.param1>0)
+						result[st] += `【${skill.cond.param1}Chain或以上】`;
+					else
+						result[st] += `【${skill.cond.param2}Chain或以下】`;
+				}
 			}
 
 			if(skill.delay.cond) {
@@ -16,11 +21,10 @@ module.exports = (card) => {
 			}
 
 			for(let role of skill.role) {
-				if(role.info.type == 1) {
-					let p = role.params;
-					true;
-					result[st] += `${p[5]}回${show(p[9])}攻击，${show(p[8])}属性，${p[7]}%暴击机率，${skill.info.chain}%Chain威力<br>伤害值：${Math.ceil(~~p[1]+~~p[2]/1000*card.info.max.level)}(${p[1]}+${p[2]/1000}*等级) + ${p[3]/10}%${show(p[6])}<br>`;
-				}
+				let p = role.params;
+
+				if(role.info.type == 1)
+					result[st] += `${p[5]}回${show(p[9])}攻击，${show(p[8])}属性，${p[7]}%暴击率，${skill.info.chain}%Chain威力<br>伤害值：${Math.ceil(~~p[1]+~~p[2]/1000*card.info.max.level)}(${p[1]}+${p[2]/1000}*等级) + ${p[3]/10}%${show(p[6])}<br>`;
 				else
 					result[st] += '暂未渲染技能'+role.info.type+'&gt;_&lt;<br>';
 			}
