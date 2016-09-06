514bf83335cd7df5c7495c08e20fe7123dd72715
DanoR <zheung@vip.qq.com>
2016-09-06 15:35:28 +0800

    render improve

--- render/skill.js
+++ render/skill.js
@@ -23,14 +23,24 @@ module.exports = (card) => {
 			for(let role of skill.role) {
 				let p = role.params;
 
-				if(role.info.type == 1)
-					result[st] += `${p[5]}回${show(p[9])}攻击，${show(p[8])}属性，${p[7]}%暴击率，${skill.info.chain}%Chain威力<br>伤害值：${Math.ceil(~~p[1]+~~p[2]/1000*card.info.max.level)}(${p[1]}+${p[2]/1000}*等级) + ${p[3]/10}%${show(p[6])}<br>`;
+				if(role.info.type == 1) {
+
+					let base = Math.ceil(~~p[1]+~~p[2]/1000*card.info.max.level);
+
+					result[st] += `<tud title="基础伤害：${p[1]}+${p[2]/1000}*等级">${base}</tud>+
+					${p[3]/10}%${show(p[6])} 的 ${p[5]}回${show(p[8])}属性
+					<tud title="${p[7]}%暴击率；${skill.info.chain}%Chain威力">${show(p[9])}攻击</tud>；
+					<br>`;
+				}
 				else
 					result[st] += '暂未渲染技能'+role.info.type+'&gt;_&lt;<br>';
 			}
 		}
-
 	}
+
+	result.awaken = result.awaken.replace(/\t|\n/g, '');
+	result.normal = result.normal.replace(/\t|\n/g, '');
+	result.support = result.support.replace(/\t|\n/g, '');
 
 	return result || '无';
 };
