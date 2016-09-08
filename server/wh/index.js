8158e9d53c8eb40962e693684b2d02f26bc28436
DanoR <zheung@vip.qq.com>
2016-09-09 00:35:48 +0800

    server improved

--- server/wh/index.js
+++ server/wh/index.js
@@ -1,6 +1,8 @@
 module.exports = () => {
 	let app = koa(), router = require('koa-router')();
 
+	let time = new Date();
+
 	router.post('/p', function() {
 		_l('webhook');
 
@@ -9,8 +11,9 @@ module.exports = () => {
 		this.body = 'webhook';
 	});
 
-	let time = new Date();
-	router.get('/t', function() { this.body = time; });
+	router.get('/t', function() {
+		this.body = Math.round((new Date().getTime() - time.getTime()) / 1000);
+	});
 
 	app.use(router.routes()).use(router.allowedMethods());
 
