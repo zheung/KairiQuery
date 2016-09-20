0f13c8e5bbffe548ca32747bfe94fafafbea05fb
DanoR <zheung@vip.qq.com>
2016-09-20 10:49:00 +0800

    tager updated

--- tagser.js
+++ tagser.js
@@ -1,6 +1,7 @@
 let func = rm('dataTag', 'func'),
 	tagList = [
-		'f.aoe', 'f.db2', 'f.covering', 'f.buffone', 'f.stan', 'f.debuffre'
+		'f.aoe', 'f.db2', 'f.covering', 'f.buffone', 'f.stan', 'f.debuffre',
+		'd.5331', 'd.linkage', 'd.limited', 'd.speical'
 	];
 
 module.exports = () => {
@@ -15,8 +16,8 @@ module.exports = () => {
 
 			if(tag[0] == 'f')
 				isExist = func[tag[1]](card);
-			else if(tag[1] == 'd')
-				isExist = rm('dataTag', tag[1])[card];
+			else if(tag[0] == 'd')
+				isExist = rm('dataTag', tag[1])[card.id];
 
 			if(isExist) tags[tag[1]] = true;
 		}
