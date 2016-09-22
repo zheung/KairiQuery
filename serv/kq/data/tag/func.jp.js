a08ea49ff3e22758ac5e92498fefb93b8b9f7879
DanoR <zheung@vip.qq.com>
2016-09-22 17:06:30 +0800

    tagser updated

---
+++ serv/kq/data/tag/func.jp.js
@@ -0,0 +1,72 @@
+let skillTypes = ['awaken', 'normal', 'support'];
+
+module.exports = {
+	aoe: (card) => {
+		for(let st of skillTypes) {
+			let first = card.skill[st][0];
+
+			for(let skill of card.skill[st])
+				for(let role of skill.role) {
+					let t = { r: role.info.target, s: skill.info.target || first.info.target };
+
+					if(role.info.type == 1)
+						if(t.r == 4 || (t.r == 1 && t.s == 5))
+							return true;
+				}
+		}
+
+		return false;
+	},
+	db2: (card) => {
+		for(let st of skillTypes)
+			for(let skill of card.skill[st])
+				for(let role of skill.role)
+					if(role.info.type == 44 && role.params[1] == '2')
+						return true;
+
+		return false;
+	},
+	covering: (card) => {
+		for(let st of skillTypes)
+			for(let skill of card.skill[st])
+				for(let role of skill.role)
+					if(role.info.type == 64)
+						return true;
+
+		return false;
+	},
+	buffone: (card) => {
+		for(let st of skillTypes) {
+			let first = card.skill[st][0];
+
+			for(let skill of card.skill[st])
+				for(let role of skill.role) {
+					let t = { r: role.info.target, s: skill.info.target || first.info.target };
+
+					if(role.info.type == 19)
+						if(t.r == 1 && t.s == 2)
+							return true;
+				}
+		}
+
+		return false;
+	},
+	stan: (card) => {
+		for(let st of skillTypes)
+			for(let skill of card.skill[st])
+				for(let role of skill.role)
+					if(role.info.type == 35)
+						return true;
+
+		return false;
+	},
+	debuffre: (card) => {
+		for(let st of skillTypes)
+			for(let skill of card.skill[st])
+				for(let role of skill.role)
+					if(role.info.type == 46)
+						return true;
+
+		return false;
+	}
+};
--- serv/kq/tagser.js
+++ serv/kq/tagser.js
@@ -1,15 +1,19 @@
 module.exports = ($) => {
-	let tags = {};
+	let tags = {}, tagList;
 
-	if($.conf.tagser) {
-		let func = $.rq('data/tag/func'),
-			tagList = [
-				'f.aoe', 'f.db2', 'f.covering', 'f.buffone', 'f.stan', 'f.debuffre',
-				'd.5331', 'd.linkage', 'd.limited', 'd.speical'
-			];
+	if($.conf.tagser)
+		tagList = [
+			'f.aoe', 'f.db2', 'f.covering', 'f.buffone', 'f.stan', 'f.debuffre',
+			'd.5331', 'd.linkage', 'd.limited', 'd.speical'
+		];
 
-		for(let serv of $.conf.servs) {
-			let tagser = {}, result = {};
+	for(let serv of $.conf.servs) {
+		let tagser, result = {};
+
+		if($.conf.tagser) {
+			let func = $.rq(`data/tag/func.${serv}`);
+
+			tagser = {};
 
 			for(let card of $.data[serv]) {
 				let tags = {};
@@ -21,7 +25,7 @@ module.exports = ($) => {
 					if(type == 'f')
 						isExist = func[tag](card);
 					else if(type == 'd')
-						isExist = $.rq(`data/tag/dict/${tag}.json`)[card.id];
+						isExist = $.rq(`data/tag/dict/${serv}/${tag}.json`)[card.id];
 
 					if(isExist)
 						tags[tag] = true;
@@ -30,26 +34,25 @@ module.exports = ($) => {
 				tagser[card.id] = Object.keys(tags);
 			}
 
-			fs.writeFileSync($.pa(`data/tagser.${serv}.json`), JSON.stringify(tagser, null, '\t'));
+			fs.writeFileSync($.pa(`data/tags.${serv}.json`), JSON.stringify(tagser, null, '\t'));
+		}
+		else
+			tagser = $.rq(`data/tags.${serv}.json`);
 
-			for(let ri in tagser) {
-				let n = {};
+		for(let ri in tagser) {
+			let n = {};
 
-				for(let t of tagser[ri])
-					n[t] = true;
+			for(let t of tagser[ri])
+				n[t] = true;
 
-				result[ri] = n;
-			}
-
-			tags[serv] = result;
+			result[ri] = n;
 		}
 
-		_l('retagser completed');
+		tags[serv] = result;
 	}
-	else
-		for(let serv of $.conf.servs)
-			tags[serv] = $.rq(`data/data.${serv}.json`);
 
+	if($.conf.tagser)
+		_l('retagser completed');
 
 	return tags;
 };
