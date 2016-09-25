14dc9257f444fc2171d83e6e24adc7edb8a6c5ee
DanoR <zheung@vip.qq.com>
2016-09-25 15:45:13 +0800

    tweak

--- serv/wh/webhook.sh
+++ serv/wh/webhook.sh
@@ -1,4 +1,5 @@
 cd ~/kq
-fr stop index.js
-git pull github master
-fr start -o log/out.log -e log/err.log index.js
+date +"%y-%m-%d %H:%M:%S" >log/bsh.log
+fr stop index.js 1>>log/bsh.log 2>log/bsh.log
+git pull github master 1>>log/bsh.log 2>>log/bsh.log
+fr start -o log/out.log -e log/err.log index.js 1>>log/bsh.log 2>>log/bsh.log
