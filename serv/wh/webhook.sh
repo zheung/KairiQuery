1e624cbff84a31cec82d853c3bd9eb9553996551
DanoR <zheung@vip.qq.com>
2016-11-01 11:14:22 +0800

    sh updated

--- serv/wh/webhook.sh
+++ serv/wh/webhook.sh
@@ -2,4 +2,5 @@ cd ~/kq
 date +"%y-%m-%d %H:%M:%S" >log/bsh.log
 fr stop index.js 1>>log/bsh.log 2>log/bsh.log
 git pull github master 1>>log/bsh.log 2>>log/bsh.log
+npm i 1>>log/bsh.log 2>>log/bsh.log
 fr start -o log/out.log -e log/err.log index.js 1>>log/bsh.log 2>>log/bsh.log
