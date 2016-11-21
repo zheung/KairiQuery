3214ffbf442044bc4888802bf54c8cede8faa5c2
DanoR <zheung@vip.qq.com>
2016-11-21 11:23:04 +0800

    file struct updated

--- serv/pub/index.js
+++
@@ -1,7 +0,0 @@
-let static = require('koa-static');
-
-module.exports = ($) => {
-	let app = koa(), router = koaRouter();
-
-	return app.use(static($.pa('asset'))).use(router.routes()).use(router.allowedMethods());
-};
